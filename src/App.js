23ca1b297b6e0b5d4b73c87e50edf4e06f046496
0Faizan <sirfaizan9@gmail.com>
2024-03-08 19:10:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Buba aur Palak ek dosre se bahut pyaar krte hai❤️❤️❤️❤️....</h1>
+      
+        
+    </div>
+  );
+}
+
+export default App;
